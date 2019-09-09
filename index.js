a2e71fb45710cfbdffd2ce93bf0ec6a952ad69b1
Filip Ilievski <ilievski.filip.mk@gmail.com>
2019-09-09 19:43:17 +0200

    Try marked options

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-var marked = require('marked');
-var yaml = require('yaml-front-matter');
-var loaderUtils = require('loader-utils');
-
+const marked = require('marked');
+const yaml = require('yaml-front-matter');
+const loaderUtils = require('loader-utils');
 
 module.exports = function(source) {
   this.cacheable && this.cacheable();
   var options = loaderUtils.parseQuery(this.query);
   var obj = yaml.parse(source);
+
+  marked.setOptions({
+    highlight(code) {
+      return require('highlight.js').highlightAuto(code).value;
+    }
+  });
+
   obj.__content = marked(obj.__content, options);
   return 'module.exports = ' + JSON.stringify(obj);
 };
